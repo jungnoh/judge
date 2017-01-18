993e0f791a3a5e7e64c8db6a4f3b02fd5578b494
studiodoth <studiodoth@protonmail.com>
2017-01-18 03:30:59 -0800

    Fixed typo

--- front/sql.js
+++ front/sql.js
@@ -31,7 +31,7 @@ module.exports = {
         callback(poolError,null);
         return;
       }
-      conn.query('INSERT INTO `submit_history` (`submit_user_id`, `problem_id`, `lang`) VALUES ('+mysql.escape(problem_id)+', '+mysql.escape(sumbit_user_id)+','+mysql.esacpe(id)+')',
+      conn.query('INSERT INTO `submit_history` (`submit_user_id`, `problem_id`, `lang`) VALUES ('+mysql.escape(problem_id)+', '+mysql.escape(submit_user_id)+','+mysql.esacpe(id)+')',
       function(err,result) {
         if(err) {
           logger.logException(err,2);
