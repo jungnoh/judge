6e6d41840d08e26669e4507a819a8d82588dfc35
studiodoth <studiodoth@protonmail.com>
2017-01-18 17:33:37 -0800

    improved logger

--- front/tools/logger.js
+++ front/tools/logger.js
@@ -1,13 +1,11 @@
 module.exports = {
   logMessage: function(msg,errorLevel) {
-    if(errorLevel==0) console.log('[LOG ]'+msg);
-    else if(errorLevel==1) console.log('[WARN]'+msg);
+    if(errorLevel===0) console.log('[LOG ]'+msg);
+    else if(errorLevel===1) console.log('[WARN]'+msg);
     else console.log('[ERR ]'+msg);
   },
   logException: function(err,errorLevel) {
-    if(errorLevel==0) console.log('[LOG ]'+err);
-    else if(errorLevel==1) console.log('[WARN]'+err);
-    else console.log('[ERR ]'+err);
+    logMessage(err,errorLevel);
     console.log(new Error().stack);
   }
 }
