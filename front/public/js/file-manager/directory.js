c74304b1d9e670efb4d876bf5ce76cd200daa2a4
studiodoth <studiodoth@protonmail.com>
2017-02-13 01:02:06 +0900

    Added New/Upload File to context menu

--- front/public/js/file-manager/directory.js
+++ front/public/js/file-manager/directory.js
@@ -23,73 +23,72 @@ window.onload = function() {
       success: function(response) {
         uploading=false;
         document.getElementById('upload-file').innerHTML='upload file';
-        alert(response);
         location.reload();
       }
     });
   });
-};
-$(function() {
-    $.contextMenu({
-        selector: '.right-menu-child',
-        callback: function(key, options) {
-          if(key==='edit') {
-            location.href=options.$trigger[0].dataset["editurl"];
-          } else if(key=='download') {
-            location.href=options.$trigger[0].dataset["downloadurl"];
-          } else if(key=='delete') {
-            $.ajax(
-              { url: options.$trigger[0].dataset["deleteurl"],
-                success: function(result){
-                  alert(result);
+  $(function() {
+      $.contextMenu({
+          selector: '.right-menu-child',
+          callback: function(key, options) {
+            if(key==='edit') {
+              location.href=options.$trigger[0].dataset["editurl"];
+            } else if(key=='download') {
+              location.href=options.$trigger[0].dataset["downloadurl"];
+            } else if(key=='delete') {
+              $.ajax(
+                { url: options.$trigger[0].dataset["deleteurl"],
+                  success: function(result){
+                    location.reload();
+                  },
+                  error: function(result) {
+                    alert(result);
+                  }
+                });
+            }
+          },
+          items: {
+              "edit": {name: "Edit", icon: "edit"},
+              "download": {name: "Download", icon: "copy"},
+              "sep1": "---------",
+              "delete": {name: "Delete", icon: "delete"}
+          }
+      });
+  });
+  $(function() {
+      $.contextMenu({
+          selector: 'body',
+          callback: function(key, options) {
+            if(key==='mkdir') {
+              var name = prompt("Enter new folder name", "");
+              var url = location.href.replace(/\/browse/,'/mkdir');
+              if(url[url.length-1]!=='/') url=url+'/';
+              url=url+name;
+              $.ajax(
+                { url: url,
+                success: function(result) {
                   location.reload();
                 },
                 error: function(result) {
-                  alert(result);
+                  alert(JSON.stringify(result));
                 }
               });
-          }
-        },
-        items: {
-            "edit": {name: "Edit", icon: "edit"},
-            "download": {name: "Download", icon: "copy"},
-            "sep1": "---------",
-            "delete": {name: "Delete", icon: "delete"}
-        }
-    });
-
-    $('.context-menu-one').on('click', function(e){
-        alert(JSON.stringify(e));
-        console.log('clicked', this);
-    })
-});
-
-$(function() {
-    $.contextMenu({
-        selector: 'body',
-        callback: function(key, options) {
-          var name = prompt("Enter new folder name", "");
-          var url = location.href.replace(/\/browse/,'/mkdir');
-          if(url[url.length-1]!=='/') url=url+'/';
-          url=url+name;
-          $.ajax(
-            { url: url,
-            success: function(result) {
-              alert(result);
-              location.reload();
-            },
-            error: function(result) {
-              alert(JSON.stringify(result));
+            } else if(key=='new') {
+              document.getElementById('new-file').click();
+            } else {
+              document.getElementById('upload-file').click();
             }
-          });
-        },
-        items: {
-            "mkdir": {name: "New Folder", icon: "add"}
-        }
-    });
+          },
+          items: {
+              "mkdir": {name: "New Folder", icon: "add"},
+              "new": {name: "New File", icon: "copy"},
+              "upload": {name: "Upload Files", icon: "paste"}
+          }
+      });
 
-    $('.context-menu-one').on('click', function(e){
-        alert(JSON.stringify(e));
-        console.log('clicked', this);
-    })
-});
+      $('.context-menu-one').on('click', function(e){
+          alert(JSON.stringify(e));
+          console.log('clicked', this);
+      })
+  });
+};
