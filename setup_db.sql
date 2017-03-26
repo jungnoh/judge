6ff9e8332bd4aeb735db259a24839a2927a6804d
studiodoth <studiodoth@protonmail.com>
2017-03-26 20:10:29 +0900

    Updated setup sql

--- setup_db.sql
+++ setup_db.sql
@@ -1,5 +1,27 @@
 CREATE DATABASE `judge`;
 USE `judge`;
+CREATE TABLE `board_comment` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(32) NOT NULL,
+  `author_id` int(32) NOT NULL,
+  `author_nick` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `upload_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+CREATE TABLE `board_post` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `author` int(32) NOT NULL,
+  `author_nick` varchar(25) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `title` varchar(300) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `views` int(32) NOT NULL DEFAULT '0',
+  `comment_count` int(32) NOT NULL DEFAULT '0',
+  `upload_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `subject` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `subject_problem` int(32) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 CREATE TABLE `languages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -11,6 +33,7 @@ CREATE TABLE `languages` (
   `source_name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+INSERT INTO `languages` VALUES (1,'C++','cpp',1,'ace/mode/c_cpp','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cpprun\",\"/workspace/runner\",\"{2}\",\"{3}\",\"-m\"]','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cppbuild\",\"/workspace/runner\"]','source.cpp'),(2,'C++11','cpp11',1,'ace/mode/c_cpp','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cpp11run\",\"/workspace/runner\",\"{2}\",\"{3}\",\"-m\"]','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cpp11build\",\"/workspace/runner\"]','source.cpp'),(3,'C++14','cpp14',1,'ace/mode/c_cpp','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cpp14run\",\"/workspace/runner\",\"{2}\",\"{3}\",\"-m\"]','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cpp14build\",\"/workspace/runner\"]','source.cpp'),(4,'C99','c99',1,'ace/mode/c_cpp','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"c99run\",\"/workspace/runner\",\"{2}\",\"{3}\",\"-m\"]','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"c99build\",\"/workspace/runner\"]','source.c');
 CREATE TABLE `problem_stats` (
   `problem_id` int(11) NOT NULL,
   `submit_count` int(11) NOT NULL DEFAULT '0',
@@ -44,7 +67,7 @@ CREATE TABLE `problems` (
   `active` tinyint(1) NOT NULL DEFAULT '0',
   `type` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 CREATE TABLE `submit_history` (
   `submit_id` int(11) NOT NULL AUTO_INCREMENT,
   `submit_user_id` int(11) NOT NULL,
@@ -63,7 +86,7 @@ CREATE TABLE `types` (
   `title` varchar(500) NOT NULL,
   `description` mediumtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `id` varchar(20) NOT NULL,
@@ -86,4 +109,3 @@ CREATE TABLE `users` (
   `ac_rate` double GENERATED ALWAYS AS (coalesce(((`ac_count` * 100) / nullif(`submit_count`,0)),0)) VIRTUAL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8;
-INSERT INTO `languages` VALUES (1,'C++','cpp',1,'ace/mode/c_cpp','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cpprun\",\"/workspace/runner\",\"{2}\",\"{3}\",\"-m\"]','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cppbuild\",\"/workspace/runner\"]','source.cpp'),(2,'C++11','cpp11',1,'ace/mode/c_cpp','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cpp11run\",\"/workspace/runner\",\"{2}\",\"{3}\",\"-m\"]','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cpp11build\",\"/workspace/runner\"]','source.cpp'),(3,'C++14','cpp14',1,'ace/mode/c_cpp','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cpp14run\",\"/workspace/runner\",\"{2}\",\"{3}\",\"-m\"]','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"cpp14build\",\"/workspace/runner\"]','source.cpp'),(4,'C99','c99',1,'ace/mode/c_cpp','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"c99run\",\"/workspace/runner\",\"{2}\",\"{3}\",\"-m\"]','[\"run\",\"-m=2G\",\"--network=none\",\"-v={0}/judge_tmp/{1}:/judgeData\",\"c99build\",\"/workspace/runner\"]','source.c');
